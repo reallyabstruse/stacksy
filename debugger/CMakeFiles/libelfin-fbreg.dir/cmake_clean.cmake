d436be851d0a34c0b1c72af33794b4c0f29e506a
reallyabstruse <59004470+reallyabstruse@users.noreply.github.com>
2022-04-06 14:28:47 +0200

    Remove files from .gitignore

--- debugger/CMakeFiles/libelfin-fbreg.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/libelfin-fbreg"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/libelfin-fbreg.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
